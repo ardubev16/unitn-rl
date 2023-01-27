13d07ca1ded6209c30c0117765ba5d0701eeeec3
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2023-01-27 19:39:06 +0100

    style: lab_2 lint

--- lab_2/project/project.srcs/sources_1/new/fade_control.vhd
+++ lab_2/project/project.srcs/sources_1/new/fade_control.vhd
@@ -3,97 +3,93 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity fade_control is
-  generic (
-    pwm_bit  : integer  --numero di bit per i livelli di intensita'
-  );
-  port (
-    clk            : in  std_logic;
-    res            : in  std_logic;
-    -- Selettore del tipo di effetto: 0 = sawtooth fade, 1 = triangular fade
-    fade_type      : in  std_logic;
-    -- Uscita del valore di intensita' (da collegare al generatore di PWM)
-    fade_intensity : out std_logic_vector( pwm_bit - 1 downto 0 )
-  );
+    generic (
+        pwm_bit  : integer  --numero di bit per i livelli di intensita'
+    );
+    port (
+        clk            : in  std_logic;
+        res            : in  std_logic;
+        -- Selettore del tipo di effetto: 0 = sawtooth fade, 1 = triangular fade
+        fade_type      : in  std_logic;
+        -- Uscita del valore di intensita' (da collegare al generatore di PWM)
+        fade_intensity : out std_logic_vector( pwm_bit - 1 downto 0 )
+    );
 end fade_control;
 
 architecture Behavioral of fade_control is
+    -- Definire segnali per
+    -- tramite un contatore abilitare le operazioni del fade control solo ogni 7.8125 ms
+    signal clk_counter : unsigned( 19 downto 0 );
+    signal clk_enable  : std_logic;
 
-  -- Definire segnali per
-  --   tramite un contatore abilitare le operazioni del fade control solo ogni 7.8125 ms
-  signal clk_counter : unsigned( 19 downto 0 );
-  signal clk_enable  : std_logic;
-
-  --   creare due effetti di modulazione
-       -- sawtooth fade : modulazione a dente di sega (loop intensity: 0->max)
-       -- triangular fade : modulazione triangolare (loop intensity:  0->max->0) 
-  signal current_intensity : unsigned( pwm_bit - 1 downto 0 );
-  signal triangular_direction : std_logic;
-  
-  -- Se si implementa una macchina a stati per controllare il generatore,
-  -- definire il tipo per gli stati ed il segnale di stato
-  
+    -- creare due effetti di modulazione
+    -- sawtooth fade : modulazione a dente di sega (loop intensity: 0->max)
+    -- triangular fade : modulazione triangolare (loop intensity:  0->max->0) 
+    signal current_intensity : unsigned( pwm_bit - 1 downto 0 );
+    signal triangular_direction : std_logic;
+    -- Se si implementa una macchina a stati per controllare il generatore,
+    -- definire il tipo per gli stati ed il segnale di stato
 begin
-
-  -- Si usa un contatore per attivare un segnale ogni 7.8125 ms. Definire qui il
-  -- processo che controlla il contatore e genera il segnale
-  fade_enable_gen : process( clk, res ) begin
-    if res = '0' then
-      clk_counter <= (others => '0');
-      clk_enable <= '0';
-    elsif rising_edge( clk ) then
-      -- if clk_counter < 781250 then
-      if clk_counter < 128 then
-        clk_counter <= clk_counter + 1;
-        clk_enable <= '0';
-      else
-        clk_counter <= (others => '0');
-        clk_enable <= '1';
-      end if;
-    end if;
-  end process;
-
-  -- Definire un processo che generi il pattern desiderato, sulla base
-  -- dell'ingresso fade_type. Qui potrebbe essere utile avere uno stato per
-  -- sapere se si sta generando la forma a sawtooth, oppure se si e' nella fase
-  -- ascendente della forma d'onda triangolare, o in quella discendente.
-  -- L'intensita' generata da questo modulo potrebbe essere per esempio
-  -- memorizzata in un contatore che possa contare in avanti e all'indietro.
-  fade_gen : process( clk, res ) begin
-      if res = '0' then
-          current_intensity <= (others => '0');
-          triangular_direction <= '0';
-      elsif rising_edge( clk ) then
-          if clk_enable = '1' then
-              if fade_type = '0' then
-          -- sawtooth fade
-                  if current_intensity < 2**pwm_bit - 1 then
-                      current_intensity <= current_intensity + 1;
-                  else
-                      current_intensity <= (others => '0');
-                  end if;
-              else
-            -- triangular fade
-                  if triangular_direction = '0' then
-              -- ascending
-                      if current_intensity < 2**pwm_bit - 1 then
-                          current_intensity <= current_intensity + 1;
-                      else
-                          current_intensity <= current_intensity - 1;
-                          triangular_direction <= '1';
-                      end if;
-                  else
-              -- descending
-                      if current_intensity > 0 then
-                          current_intensity <= current_intensity - 1;
-                      else
-                          current_intensity <= current_intensity + 1;
-                          triangular_direction <= '0';
-                      end if;
-                  end if;
-              end if;
-          end if;
+    -- Si usa un contatore per attivare un segnale ogni 7.8125 ms. Definire qui il
+    -- processo che controlla il contatore e genera il segnale
+    fade_enable_gen : process( clk, res ) begin
+        if res = '0' then
+            clk_counter <= (others => '0');
+            clk_enable <= '0';
+        elsif rising_edge( clk ) then
+            if clk_counter < 781250 then
+              -- if clk_counter < 128 then
+                clk_counter <= clk_counter + 1;
+                clk_enable <= '0';
+            else
+                clk_counter <= (others => '0');
+                clk_enable <= '1';
+            end if;
         end if;
-      end process;
+    end process;
 
-      fade_intensity <= std_logic_vector( current_intensity );
+    -- Definire un processo che generi il pattern desiderato, sulla base
+    -- dell'ingresso fade_type. Qui potrebbe essere utile avere uno stato per
+    -- sapere se si sta generando la forma a sawtooth, oppure se si e' nella fase
+    -- ascendente della forma d'onda triangolare, o in quella discendente.
+    -- L'intensita' generata da questo modulo potrebbe essere per esempio
+    -- memorizzata in un contatore che possa contare in avanti e all'indietro.
+    fade_gen : process( clk, res ) begin
+        if res = '0' then
+            current_intensity <= (others => '0');
+            triangular_direction <= '0';
+        elsif rising_edge( clk ) then
+            if clk_enable = '1' then
+                if fade_type = '0' then
+                    -- sawtooth fade
+                    if current_intensity < 2**pwm_bit - 1 then
+                        current_intensity <= current_intensity + 1;
+                    else
+                        current_intensity <= (others => '0');
+                    end if;
+                else
+                    -- triangular fade
+                    if triangular_direction = '0' then
+                        -- ascending
+                        if current_intensity < 2**pwm_bit - 1 then
+                            current_intensity <= current_intensity + 1;
+                        else
+                            current_intensity <= current_intensity - 1;
+                            triangular_direction <= '1';
+                        end if;
+                    else
+                        -- descending
+                        if current_intensity > 0 then
+                            current_intensity <= current_intensity - 1;
+                        else
+                            current_intensity <= current_intensity + 1;
+                            triangular_direction <= '0';
+                        end if;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    fade_intensity <= std_logic_vector( current_intensity );
 end Behavioral;
--- lab_2/project/project.srcs/sources_1/new/led_pwm.vhd
+++ lab_2/project/project.srcs/sources_1/new/led_pwm.vhd
@@ -3,90 +3,77 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity led_pwm is
-  generic (
-    -- Numero di bit del contatore del PWM
-    pwm_bit : integer := 7
-  );
-  port (
-    clk              : in  std_logic;
-    res              : in  std_logic;
-    -- Ingressi per controllare la luminosita'
-    dimmer_up        : in  std_logic;
-    dimmer_down      : in  std_logic;
-    -- Ingresso di valore di intensita' esterno, da collegare al generatore di
-    -- pattern (il fade_control)
-    intensity_ext    : in  std_logic_vector( pwm_bit - 1 downto 0 );
-    -- Ingresso di selezione tra l'uso del fade control oppure l'impostazione
-    -- manuale
-    manual_select    : in  std_logic;
-    -- Uscita per mostrare l'effettivo valore di intensita' impiegato (da
-    --- mandare eventualmente ai display)
-    actualIntensity  : out std_logic_vector( pwm_bit - 1 downto 0 );
-    -- Uscita PWM che controlla il LED
-    led_pwr          : out std_logic
-  );
+    generic (
+        -- Numero di bit del contatore del PWM
+        pwm_bit : integer := 7
+    );
+    port (
+        clk              : in  std_logic;
+        res              : in  std_logic;
+        -- Ingressi per controllare la luminosita'
+        dimmer_up        : in  std_logic;
+        dimmer_down      : in  std_logic;
+        -- Ingresso di valore di intensita' esterno, da collegare al generatore di
+        -- pattern (il fade_control)
+        intensity_ext    : in  std_logic_vector( pwm_bit - 1 downto 0 );
+        -- Ingresso di selezione tra l'uso del fade control oppure l'impostazione
+        -- manuale
+        manual_select    : in  std_logic;
+        -- Uscita per mostrare l'effettivo valore di intensita' impiegato (da
+        --- mandare eventualmente ai display)
+        actualIntensity  : out std_logic_vector( pwm_bit - 1 downto 0 );
+        -- Uscita PWM che controlla il LED
+        led_pwr          : out std_logic
+    );
 end led_pwm;
 
 -- Definizione dell'architettura del modulatore PWM.
 architecture Behavioral of led_pwm is
-
-  -- Definire segnali per il PWM, per esempio un contatore, e per tenere traccia
-  -- del livello di intensita' da impostare tramite i bottoni.
-  signal counter : unsigned( pwm_bit - 1 downto 0 );
-  signal soglia  : unsigned( pwm_bit - 1 downto 0 );
-
+    -- Definire segnali per il PWM, per esempio un contatore, e per tenere traccia
+    -- del livello di intensita' da impostare tramite i bottoni.
+    signal counter : unsigned( pwm_bit - 1 downto 0 );
+    signal soglia  : unsigned( pwm_bit - 1 downto 0 );
 begin
-
-  -- Definire un processor che implementi un contatore free running
-  pwm_counter: process( clk, res ) begin
-    if res = '0' then
-      counter <= (others => '0');
-    elsif rising_edge( clk ) then
-      counter <= counter + 1;
-    end if;
-  end process;
-
-  -- Definire un processor che aggiorni il valore dell'uscita led_pwr a seconda
-  -- dei valori del contatore e della intensita' desiderata
-  pwm_control: process( clk ) begin
-    if rising_edge( clk ) then
-      if counter < soglia then
-        led_pwr <= '1';
-      else
-        led_pwr <= '0';
+    -- Definire un processor che implementi un contatore free running
+    pwm_counter: process( clk, res ) begin
+        if res = '0' then
+            counter <= (others => '0');
+        elsif rising_edge( clk ) then
+            counter <= counter + 1;
         end if;
-    end if;
-  end process;
+    end process;
 
-  -- Definire un processo che sulla base dei bottoni di comando aggiorni il
-  -- valore dell'intensita' desiderata
-  intensity_control: process( clk ) begin
-    if rising_edge( clk ) then
-      if manual_select = '0' then
-        if dimmer_up = '1' then
-          soglia <= soglia + 1;
-        elsif dimmer_down = '1' then
-          soglia <= soglia - 1;
+    -- Definire un processor che aggiorni il valore dell'uscita led_pwr a seconda
+    -- dei valori del contatore e della intensita' desiderata
+    pwm_control: process( clk ) begin
+        if rising_edge( clk ) then
+            if counter <= soglia and soglia /= 0 then
+                led_pwr <= '1';
+            else
+                led_pwr <= '0';
+            end if;
         end if;
-      else
-        soglia <= unsigned( intensity_ext );
-      end if;
-    end if;
-  end process;
+    end process;
 
-  -- Definire processi o equazioni per selezionare il livello di intensita'
-  -- desiderata tra quella in ingresso dall'esterno oppure quella impostata dai
-  -- bottoni. Definire anche il valore dell'intensita' effettiva che viene
-  -- utilizzata da mandare al segnale actualIntensity
-  -- intensity_level: process( clk ) begin
-  --   if rising_edge( clk ) then
-  --     if manual_select = '1' then
-  --       actualIntensity <= std_logic_vector(soglia);
-  --     else
-  --       actualIntensity <= intensity_ext;
-  --     end if;
-  --   end if;
-  -- end process;
+    -- Definire un processo che sulla base dei bottoni di comando aggiorni il
+    -- valore dell'intensita' desiderata
+    intensity_control: process( clk ) begin
+        if rising_edge( clk ) then
+            if manual_select = '0' then
+                if dimmer_up = '1' then
+                    soglia <= soglia + 1;
+                elsif dimmer_down = '1' then
+                    soglia <= soglia - 1;
+                end if;
+            else
+                soglia <= unsigned( intensity_ext );
+            end if;
+        end if;
+    end process;
 
-  actualIntensity <= std_logic_vector(soglia);
+    -- Definire processi o equazioni per selezionare il livello di intensita'
+    -- desiderata tra quella in ingresso dall'esterno oppure quella impostata dai
+    -- bottoni. Definire anche il valore dell'intensita' effettiva che viene
+    -- utilizzata da mandare al segnale actualIntensity
+    actualIntensity <= std_logic_vector(soglia);
 end Behavioral;
